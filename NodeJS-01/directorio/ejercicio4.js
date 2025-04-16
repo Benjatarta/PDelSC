9ffdab80b81e4f5c6d5fc0019253db084d12a980
Benjatarta <144744803+Benjatarta@users.noreply.github.com>
2025-04-15 22:36:33 -0300

    ejercicio4.js

--- NodeJS-01/directorio/ejercicio4.js
+++ NodeJS-01/directorio/ejercicio4.js
@@ -1,7 +1,7 @@
 //importo variables de otro archivo
 import { suma, resta, multi, division } from './calculos.js';
 
-    console.log(suma (4,5));
-    console.log(resta (3,6));
-    console.log(multi (2,7));
-    console.log(division (20,4));
+    console.log(suma (5,3));
+    console.log(resta (8,6));
+    console.log(multi (3,11));
+    console.log(division (30,5));
